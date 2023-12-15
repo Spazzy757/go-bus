78138197a97466f2b427e2b7e95dbe412573ac4b
Sebastian <silenteh@gmail.com>
2023-12-15 13:41:32 +0100

    First simple implementation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/re-cinq/go-bus
+
+go 1.21.5
+
+require github.com/go-logr/logr v1.3.0
---
+++ worker.go
@@ -0,0 +1,18 @@
+package bus
+
+// worker is spawned in a go routine and
+type worker struct {
+
+	// The worker own channel
+	data chan Event
+
+	// Waiting for the worker to conclude its tasks
+	closing chan bool
+}
+
+func newWorker() worker {
+	return worker{
+		data:    make(chan Event, 1),
+		closing: make(chan bool, 1),
+	}
+}
